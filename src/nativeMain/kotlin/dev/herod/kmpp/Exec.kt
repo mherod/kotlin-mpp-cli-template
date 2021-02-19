365944564cb91a34a67a26231453dbafb5b9df1e
Matthew Herod <matthew.herod@myunidays.com>
2021-02-19 20:02:01 +0000

    Adjusted implementation of exec which should lend itself to better flow parallelisation

--- src/nativeMain/kotlin/dev/herod/kmpp/Exec.kt
+++ src/nativeMain/kotlin/dev/herod/kmpp/Exec.kt
@@ -1,28 +1,23 @@
 package dev.herod.kmpp
 
-import kotlinx.cinterop.ByteVar
-import kotlinx.cinterop.allocArray
+import dev.herod.kx.flow.loop
+import dev.herod.kx.flow.takeWhileNotNull
 import kotlinx.cinterop.memScoped
-import kotlinx.cinterop.toKString
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import platform.posix.fgets
+import kotlinx.coroutines.flow.*
 import platform.posix.pclose
 import platform.posix.popen
 
 actual fun exec(command: String): Flow<String> = flow {
-    if (debugLogsEnabled) println(">> $command")
-    popen(command, "r")?.let { pointer ->
-        memScoped {
-            val readBufferLength = 128
-            val buffer = allocArray<ByteVar>(readBufferLength)
-            var line = fgets(buffer, readBufferLength, pointer)?.toKString()?.trim()
-            while (line != null) {
-                if (debugLogsEnabled) println("<< $line")
-                emit(line)
-                line = fgets(buffer, readBufferLength, pointer)?.toKString()?.trim()
-            }
-        }
-        pclose(pointer)
+    val pointer = popen(command, "r")
+    memScoped {
+        emitAll(
+            flow = loop()
+                .onStart { if (debugLogsEnabled) println(">> $command") }
+                .map { readToBuffer(bufferSize = 1024, filePointer = pointer) }
+                .takeWhileNotNull()
+                .flatMapConcat { it.split("\n".toRegex()).asFlow() }
+                .onEach { if (debugLogsEnabled) println("<< $it") }
+        )
     }
+    pclose(pointer)
 }
