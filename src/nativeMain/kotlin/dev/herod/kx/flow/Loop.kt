33c3a8c75b6c7bb01c4a77d71d478d1b4b448649
Matthew Herod <matthew.herod@myunidays.com>
2021-02-19 20:10:50 +0000

    No need to increment value

--- src/nativeMain/kotlin/dev/herod/kx/flow/Loop.kt
+++ src/nativeMain/kotlin/dev/herod/kx/flow/Loop.kt
@@ -4,13 +4,9 @@ import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.isActive
-import kotlin.native.concurrent.AtomicLong
 
 fun loop(): Flow<Long> = flow {
-    val currentCoroutineContext = currentCoroutineContext()
-    val count = AtomicLong(0L)
-    while (currentCoroutineContext.isActive) {
-        count.increment()
-        emit(count.value)
+    currentCoroutineContext().run {
+        while (isActive) emit(0L)
     }
 }
